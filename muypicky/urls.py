fa1e28f4e31accc207326ac9e5ae81890967b5d2
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-09-23 16:06:34 +0530

    Display Restraunt

--- muypicky/urls.py
+++ muypicky/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
-from restraunt.views import HomeView, AboutView, ContactView
+from restraunt.views import restraunt_list,  AboutView, ContactView
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view()),
+    url(r'^restraunt/', restraunt_list),
+    url(r'^$', TemplateView.as_view(template_name='home.html')),
     url(r'^about/', AboutView.as_view()),
     url(r'^contact/', ContactView.as_view()),
 ]
--- restraunt/views.py
+++ restraunt/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView
-
+from .models import RestrauntLocation
 # Create your views here.
 
-class HomeView(TemplateView):
-    template_name = 'home.html'
-    def get_context_data(self, *args, **Kwargs):
-        context = super(HomeView, self).get_context_data(*args, **Kwargs)
-        context = {
-            "title": "Home Page",
-        }
-        return context   
+
+def restraunt_list(request):
+    queryset = RestrauntLocation.objects.all()  
+    context = {
+        "title": "Restraunts",
+        "objects": queryset
+    }
+    return render(request, "restraunt.html", context) 
 
 class AboutView(TemplateView):
     template_name = 'about.html'
