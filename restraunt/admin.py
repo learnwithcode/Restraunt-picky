6a9834fcb02c80ddbcb9c0a452c75ba390f232bc
Abhiraj Pundir <pundirabhiraj@gmai.com>
2018-09-23 15:18:34 +0530

    Creating Model & Admin

--- restraunt/admin.py
+++ restraunt/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import RestrauntLocation
 # Register your models here.
+
+
+class RestrauntAdmin(admin.ModelAdmin):
+    list_display = ["name", "location", "category", "timestamp", "updated"]
+    search_fields = ['name', 'location', 'category']
+    list_filter = ['name', 'location', 'category']
+admin.site.register(RestrauntLocation, RestrauntAdmin)    
